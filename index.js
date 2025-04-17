22a594268e136575a853e2fe5cc2bc67ba957c21
Aditya <ammuaditya303@gmail.com>
2025-04-17 20:06:03 +0530

    token change

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ app.post('/login', async (request, response) => {
             gender: dbUser.gender,
           }
           const jwtToken = jwt.sign(payload, 'userDetails')
-          response.json(sjwtToken)
+          response.json(jwtToken)
       } else {
         response.status(400)
         response.json('Invalid password')
