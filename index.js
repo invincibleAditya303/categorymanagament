9c32c35b9860b7707ae6619f1f20fef7fb871ad6
Aditya <ammuaditya303@gmail.com>
2025-04-18 12:16:07 +0530

    final change

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.post('/login', async (request, response) => {
   
     if (jwtToken === undefined) {
       response.status(401)
-      response.json(`authecationError: ${request}`)
+      response.json('Authenctication error')
     } else {
       jwt.verify(jwtToken, 'userDetails', async (error, payload) => {
         if (error) {
