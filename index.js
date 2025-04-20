b8e64faebaaed2b03588eb63f43473aaab724425
Aditya <ammuaditya303@gmail.com>
2025-04-20 19:44:02 +0530

    upload path files

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ app.use(cors({
 require('dotenv').config()
 const cloudinary = require('cloudinary').v2
 
-cloudinary.config({cloud_name: process.env.CLOUD_NAME})
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+})
 
 const multer = require('multer')
 const {CloudinaryStorage} = require('multer-storage-cloudinary')
